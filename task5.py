93f939bfdd11d818a4a3263d64dc4b9d7b8518bd
ganjabass911 <32351202+ganjabass911@users.noreply.github.com>
2018-07-09 23:09:03 +0300

    add baged algoritms

--- task5.py
+++ task5.py
@@ -27,34 +27,55 @@ import os
 import simplekml
 import xlrd
 
+# края точек в data.xml
+border = {'left': 38.7518678245754, 'right': 38.8036140188757, 'bottom': 45.0968821926186, 'top': 45.1335440675189}
+
 
 # столбец матрицы ( матрица, индекс столбца)
 def generatorCollumn(matrix, colnum):
     return [init[colnum] for init in matrix]
 
 
+# чтение таблицы из файла
+def dataInit(file):
+    sheet = xlrd.open_workbook(file, formatting_info=True).sheet_by_index(0)
+
+    # data = {}
+    # for rownum in range(sheet.nrows):
+    #     if rownum != 0:
+    #         p = int(sheet.row_values(rownum)[0]), int(sheet.row_values(rownum)[1])
+    #         data.update({p: [float(sheet.row_values(rownum)[2]),
+    #                          float(sheet.row_values(rownum)[3]), float(sheet.row_values(rownum)[4]),
+    #                          float(sheet.row_values(rownum)[5]), sheet.row_values(rownum)[6]]})
+    # return data
+    # try:
+    #     list1 =float(sheet.row_values(4)[6].split())
+    # except ValueError as e:
+    #     print( "error", e)
+
+    return tuple([int(sheet.row_values(rownum)[0]),
+                  int(sheet.row_values(rownum)[1]),
+                  float(sheet.row_values(rownum)[2]),
+                  float(sheet.row_values(rownum)[3]),
+                  float(sheet.row_values(rownum)[4]),
+                  float(sheet.row_values(rownum)[5]),
+                  float(sheet.row_values(rownum)[6])] for rownum in range(sheet.nrows) if rownum != 0)
+
+    # кортеж исходной таблицы
+
+
 # получение точек
 def generatorPoints(data):
     gen = {}
-    for i in range(len(data)):
-        if i != 0:
-            branche = data[i]
-            if not (branche[0] in gen):
-                gen.update({int(branche[0]): [branche[2], branche[3]]})
-            if not (branche[1] in gen):
-                gen.update({int(branche[0]): [branche[4], branche[5]]})
+    for road in data:
+        if not (road[0] in gen):
+            gen.update({int(road[0]): [road[2], road[3]]})
+        if not (road[1] in gen):
+            gen.update({int(road[1]): [road[4], road[5]]})
     return gen
 
 
-file = xlrd.open_workbook('data.xls', formatting_info=True)
-sheet = file.sheet_by_index(0)
-# кортеж исходной таблицы
-data = tuple(sheet.row_values(rownum) for rownum in range(sheet.nrows))
-
-# словарь точек
-points = generatorPoints(data)
-
-
+# нахождение границ
 def borderPoints(points):
     left = 180
     right = 0
@@ -68,21 +89,17 @@ def borderPoints(points):
     return {'left': left, 'right': right, 'bottom': bottom, 'top': top}
 
 
-border = {'left': 38.7518678245754, 'right': 38.8036140188757, 'bottom': 45.0968821926186, 'top': 45.1335440675189}
-
-
-# проверка ввода
-def initCheck():
-    while True:
-        x = float(input())
-        if border.get('left') <= x <= border.get('right') or border.get('bottom') <= x <= border.get('top'):
-            return x
-        else:
-            print('Ошибка! Введте корректные данные!')
-
-
 # Ввод
 def init():
+    # проверка ввода
+    def initCheck():
+        while True:
+            x = float(input())
+            if border.get('left') <= x <= border.get('right') or border.get('bottom') <= x <= border.get('top'):
+                return x
+            else:
+                print('Ошибка! Введте корректные данные!')
+
     print('Введите координаты первой точки: долгота широта')
     a = [initCheck(), initCheck()]
 
@@ -104,63 +121,110 @@ def findPoint(a, points):
             distance = dist1
 
 
-# def floidBellman(point1, point2, data, n):
-#     # m - количество ребер
-#     # n - количество вершин
-#
-#     d = [math.inf for i in range(n)]
-#     d[0] = 0
-#     for i in range[n]:
-#         for j in range:
-#             if branch!=data[0]:
-#                 if d
+# не работает должным образом
+def floidBellman(point1, data):
+    # m - количество ребер
+    # n - количество вершин
+
+    points = dict.fromkeys(generatorPoints(data).keys(), 200000)
+    points.update({point1: 0})
+    for point in points:
+        if point != point1:
+            for road in data:
+                if points.get(road[1]) > (points.get(road[0]) + float(road[6])):
+                    points.update({road[1]: points.get(road[0]) + float(road[6])})
+                if points.get(road[0]) > points.get(road[1]) + float(road[6]):
+                    points.update({road[0]: points.get(road[1]) + float(road[6])})
+
+    return points
 
 
-file = xlrd.open_workbook('data.xls', formatting_info=True)
-sheet = file.sheet_by_index(0)
-# кортеж исходной таблицы
-data = tuple(sheet.row_values(rownum) for rownum in range(sheet.nrows))
+# соседние вершины, в которые идут ребра
+def waysTo(data):
+    ways = {}
+    for way in data:
+        if not ways.get(way[0]):
+            ways.setdefault(way[0], [way[1]])
+        else:
+            c = list(ways.get(way[0]))
+            c.append(way[1])
+            ways.update({way[0]: c})
+    return ways
 
+
+# соседние вершины, из которых идут ребра
+def waysFrom(data):
+    ways = {}
+    for way in data:
+        if not ways.get(way[1]):
+            ways.setdefault(way[1], [way[0]])
+        else:
+            c = list(ways.get(way[1]))
+            c.append(way[0])
+            ways.update({way[0]: c})
+    return ways
+
+
+def dijkstra(point1, point2, data):
+    dist = dict.fromkeys(generatorPoints(data).keys(), 10000)
+    wt = waysTo(data)
+    dist.update({point1: 0})
+    parents = dict.fromkeys(generatorPoints(data).keys(), False)
+    mark = parents.copy()
+    roads = {(road[0], road[1]): road[6] for road in data}
+    roads.update({(road[1], road[0]): road[6] for road in data})
+    v = point1
+    if wt.get(v):
+        for j in wt.get(v):
+            if dist.get(j) > dist.get(v) + roads.get(v, j):
+                dist.update({j: dist.get(v) + roads.get(v, j)})
+                parents.update({v: j})
+    for i in dist.keys():
+
+        # выбирается вершина с наименьшей величиной dist.get(j)
+        for j in dist.keys():
+            if not (mark.get(j)) and (v == point1 or dist.get(j) < dist.get(v)):
+                v = j
+        # if dist.get(v) ==100:
+        #     continue
+
+        # вершина помечается
+        mark.update({v: True})
+        # просматриваем все ребра (v,to) и пытаемся улучшить значение dist.get(to)
+        if wt.get(v):
+            for j in wt.get(v):
+                road = roads.get(v, j)
+                if dist.get(j) > dist.get(v) + roads.get(v, j):
+                    dist.update({j: dist.get(v) + roads.get(v, j)})
+                    parents.update({v: j})
+
+    return dist
+
+
+data = dataInit('data.xls')
 # словарь точек
 points = generatorPoints(data)
-mx = 0
-for point in points:
-    mx = max(mx, point)
 
 # a, b = init()
-a, b = [38.98, 45.07], [39, 45.05]
+a, b = [38.75337683, 45.11878547], [38.8036140188757, 45.0968821926186]
 
 findPoint(a, points)
 findPoint(b, points)
-#
-# # a[2][0]id точки близкой к нужной точке А
+
+# roads = floidBellman(a[2], data)
+roads = dijkstra(a[2], b[2], data)
+print(roads)
 # kml = simplekml.Kml()
-# kml.newpoint(name="A", coords=[(points.get(a[2]))])  # lon, lat, optional height
-# kml.newpoint(name="B", coords=[(points.get(b[2]))])
-# kml.newpoint(name="a", coords=[(38.98, 45.07)])
-# kml.newpoint(name="b", coords=[(39, 45.05)])
-# kml.newpoint(name="0", coords=[(points.get(list(points.keys())[0]))])
+# # for road in data:
+# #     # kml.newpoint(coords=[(points.get(point))])
+# #     kml.newlinestring(description=str(road[6]), coords=[(road[2], road[3]), (road[4], road[5])])
 # kml.save("1.kml")
-
-
-
-print(border)
-k = simplekml.Kml()
-k.newpoint(name="lt", coords=[(border.get('left'), border.get('top'))])
-k.newpoint(name="lb", coords=[(border.get('left'), border.get('bottom'))])
-k.newpoint(name="rt", coords=[(border.get('right'), border.get('top'))])
-k.newpoint(name="rb", coords=[(border.get('right'), border.get('bottom'))])
-k.newlinestring(name="left", description="llllllllll",
-                  coords=[(border.get('left'), border.get('top')), (border.get('left'), border.get('bottom'))])
-k.newlinestring(name="right", description="rrrrrrrrr",
-                  coords=[(border.get('right'), border.get('top')), (border.get('right'), border.get('bottom'))])
-k.newlinestring(name="top", description="ttttttttttt",
-                  coords=[(border.get('left'), border.get('top')), (border.get('right'), border.get('top'))])
-k.newlinestring(name="bottom", description="bbbbbbbbbbbb",
-                  coords=[(border.get('left'), border.get('bottom')), (border.get('right'), border.get('bottom'))])
-k.save("border.kml")
 """
 проблема в неверном условии:
 38,9774837690821 <  долгота < 39,0283642309179
 45,0549145480683 < широта < 45,0908534519317
-"""
+
+    
+    
+    точки в файле 
+    """
